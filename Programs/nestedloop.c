47d9ccb1addfc5a4f19a349d548b9fdc1a4c371c
japmeet01 <72161668+japmeet01@users.noreply.github.com>
2020-10-09 14:36:44 +0530

    Create nestedloop.c

---
+++ Programs/nestedloop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int i,j;
+
+    for(i=1;i<=5;i++){
+        printf("\nfor i=%d",i);
+
+        for(j=0;j<2;j++){
+         printf("*");
+        }
+        }
+
+    return 0;
+}
